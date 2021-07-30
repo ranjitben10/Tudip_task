b11705b859adfaaeda8bc7e6b024b80eeea73553
Ranjit Panda <38244278+ranjitben10@users.noreply.github.com>
2021-07-30 14:56:21 +0530

    update

--- server/keys.js
+++ server/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI : "mongodb+srv://Ranjit:zapxbdkKDBQCPu1M@cluster0.4xazj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    MongoURI : "mongodb+srv://Ranjit:************@cluster0.4xazj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 }
